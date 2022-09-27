e5fbe4bb2eaa31f56bdeaf0dedf7f678e501cdab
mohammad-azilen <106139897+mohammad-azilen@users.noreply.github.com>
2022-09-27 18:02:51 +0530

    Delete pdf.js

--- pdf.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let elements = document.getElementsByClassName('invoice-entry');
-    for (let i = 0; i < elements.length; i++) {
-        if(elements[i].getElementsByTagName("td")[0].innerHTML == "BuzzBox Calls_Other") {
-            elements[i].getElementsByTagName("td")[2].innerHTML = "N/A";
-        }
-    }
-}, false);
